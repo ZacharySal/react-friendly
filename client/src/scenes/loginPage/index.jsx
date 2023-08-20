cc39349e48057acb8d75ad64d0f0e2917b3d30ce
ZacharySal <zach.salvaggio@gmail.com>
2023-08-20 00:52:36 -0500

    update

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -27,7 +27,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to Friendly, the Social Media for Sociopaths!
+          Welcome to Friendly!
         </Typography>
         {errorMsg && (
           <Alert severity="error" sx={{ fontSize: "inherit", mb: "1.5rem" }}>
