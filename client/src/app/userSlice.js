f46b0c917dd16cb1b4885ec4a05d6c54cf4e0fff
ZacharySal <zach.salvaggio@gmail.com>
2023-09-19 01:43:01 -0500

    update

--- client/src/app/userSlice.js
+++ client/src/app/userSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
-    mode: "dark",
+    mode: "light",
     user: null,
     token: null,
     error: null,
